f3cd3d688ddd7e0e0d5e7dfa675362b5664baca6
James <32406570+jimmy-jimjam@users.noreply.github.com>
2018-10-27 22:19:54 -0500

    Create Lab_4.ino

---
+++ Lab_4.ino
@@ -0,0 +1,71 @@
+//Name: James Iorgovan
+//Date: Fall 2018
+//Assignemnt: Lab 4 - More complicated input
+//Description: The display of the photoresistorshould state the relative amount of light in the room 
+//(dark, partially dark, medium, partially light, fully lit).
+//Assumptions: I assume that the photoresistor will not act up like the potentiometer did from lab 3. Also, the wiring of the lab 
+//won't exactly matter, as long as we have it similar to the photo.
+//References: Code snippet - "http://playground.arduino.cc/Learning/PhotoResistor"
+//                           "https://www.instructables.com/id/How-to-use-a-photoresistor-or-photocell-Arduino-Tu/"
+//                           "http://learn.adafruit.com/photocells"
+//                            Lab 3
+
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const int photoResistor = A0, firstLine;
+int brightness = 0, value = 0;
+
+void setup(){
+  lcd.begin(16, 2);
+  pinMode(photoResistor, INPUT);
+  Serial.begin(9600);
+}
+
+void myDisplay(String var){
+  lcd.clear();
+  lcd.print(var);
+}
+
+/*void myDisplay2(int var1){
+  lcd.clear();
+  lcd.print(var1);
+  delay(200);
+}
+*/
+void loop(){
+  
+  brightness = analogRead(photoResistor);   // read in unput
+ // myDisplay2(brightness);
+  displayValue(brightness);
+  delay(100);
+  //lcd.clear();
+}
+
+void displayValue(int brightness){
+  
+  if(brightness <= 300){
+    myDisplay("dark");
+  }
+  
+  else if(brightness <= 400){
+    myDisplay("partially dark");
+  }
+  
+  else if(brightness <= 500){
+    myDisplay("medium");
+  }
+  
+  else if(brightness <= 600){
+    myDisplay("partially lit");
+  }
+  
+  else if(brightness <= 620){
+    myDisplay("fully lit");
+  }
+
+  //myDisplay2(brightness);
+}
